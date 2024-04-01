df8e0b19cc6395a2edd5486339dced51caa6df42
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-02 03:58:28 +0600

    condtional 4

---
+++ 02-Operators-and-Conditional-Statements/condition-and-operators/condtional_4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if (tk = 5000)
+    {
+        printf("Cox's Bazar Jaboo \n");
+        if (tk >= 10000)
+            printf("Saint Martin Jaboo \n");
+        else
+        {
+            printf("Bashay Chole jaboo \n");
+        }
+    }
+    else
+    {
+        printf("Kothaw Jabona \n");
+    }
+
+    if (tk == 7000)
+    {
+        printf("muri khabo");
+    }
+
+    if (tk == 6000)
+    {
+        printf("brindabon jabo");
+    }
+    
+    return 0;
+}
