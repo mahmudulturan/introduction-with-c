c58e36c23a373e2ce4c5c4565fff7c1d12405543
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-01 23:48:00 +0600

    get input

--- 01-Basic-Syntax-Variables-and-Data-Types/c-project/input.c
+++ 01-Basic-Syntax-Variables-and-Data-Types/c-project/input.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int rahim;
-    float karim;
-    char charecter;
-    scanf("%d %f %c", &rahim, &karim, &charecter);
-    printf("%d %0.1f %c", rahim, karim, charecter);
+    // int rahim;
+    // float karim;
+    // char charecter;
+    // scanf("%d %f %c", &rahim, &karim, &charecter);
+    // printf("%d %0.1f %c", rahim, karim, charecter);
+
+    int a, b;
+    scanf("%d%% %d%%", &a, &b);
+    printf("%d%% %d%%", a, b);
     return 0;
 }
