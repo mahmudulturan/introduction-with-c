240828e5cedf0b8095987afc9d38a94c9b7efd1c
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-02 02:50:51 +0600

    conditional

---
+++ 02-Operators-and-Conditional-Statements/condition-and-operators/conditional_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if (a == 100) // if the value of a and 100 are equal then it will go the if block otherwise it will go to the else block
+    {
+        printf("Burger khabo");
+    }
+    else
+    {
+        printf("kichu khabo na");
+    }
+    return 0;
+}
