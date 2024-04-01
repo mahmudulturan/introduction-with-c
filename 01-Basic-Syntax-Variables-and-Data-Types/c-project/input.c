743df1ae70f376ee875107128d57f2bd361b8995
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-01 23:25:34 +0600

    take input

---
+++ 01-Basic-Syntax-Variables-and-Data-Types/c-project/input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int rahim;
+    float karim;
+    char charecter;
+    scanf("%d %f %c", &rahim, &karim, &charecter);
+    printf("%d %0.1f %c", rahim, karim, charecter);
+    return 0;
+}
