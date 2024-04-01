ece48400d56439b236d5a23f78882f4d6a7d86d6
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-01 22:53:30 +0600

    print variables

---
+++ 01-Basic-Syntax-Variables-and-Data-Types/c-project/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+ "a";
+    // printf("%c", theChar);
---
+++ 01-Basic-Syntax-Variables-and-Data-Types/c-project/variable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+
+    // to print integer
+    int theNumber;         // declare the variable;
+    theNumber = 4554;      // assign the value;
+    int rahim = 4465456 , karim =544; // intialize a variable;
+    printf("%d %d", rahim, karim);
+
+    // to print floating
+    float theFloat = 2.56454;
+    printf("%0.1f", theFloat);
+
+    // to print characters
+    char theChar = 'f';
+    printf("%c", theChar);
+
+    return 0;
+}
