6fec4c6e118b5e6a6fbd87df1b11108cf1711313
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-02 03:56:31 +0600

    nested condtion

---
+++ 02-Operators-and-Conditional-Statements/condition-and-operators/conditional_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if (tk = 5000)
+    {
+        printf("Cox's Bazar Jaboo \n");
+        if (tk >= 10000)
+        {
+            printf("Saint Martin Jaboo \n");
+        }
+        else
+        {
+            printf("Bashay Chole jaboo \n");
+        }
+    }
+    else
+    {
+        printf("Kothaw Jabona \n");
+    }
+    return 0;
+}
