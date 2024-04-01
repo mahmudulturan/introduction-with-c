736d0fe28a1545863d3c0fa2f1c294e68ae06bb6
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-01 19:24:21 +0600

    speacial charecters

--- 01-Basic-Syntax-Variables-and-Data-Types/c-project/index.c
+++ 01-Basic-Syntax-Variables-and-Data-Types/c-project/index.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello");
+    printf("Special Charecters \n to give a new line \n");
+    printf("Special Charecters \t to give a tab space \n");
+    printf("Special Charecters \\ to write back slash \n");
+    printf("Special Charecters \' to write a single cotation \n");
+    printf("Special Charecters \" to write a double cotation \n");
+    printf("Special Charecters %% to write a parcent \n");
     return 0;
 }
