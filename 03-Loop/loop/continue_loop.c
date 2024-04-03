2fd9ef648969eb5cc3964fa08556c77f1b2369ed
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-03 21:58:09 +0600

    continue-loop

---
+++ 03-Loop/loop/continue_loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    for (i = 1; i <= 10; i += 1)
+    {
+        if (i % 2 == 0)
+        {
+            printf("%d - Even \n", i);
+        }
+        else if (i == 7)
+        {
+            continue;;
+        }
+        else{
+            printf("%d - Odd \n", i);
+        }
+    }
+    return 0;
+}
