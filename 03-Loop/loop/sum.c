abb2fb083b801204cd7899b5b1932e47e4446654
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-03 21:29:29 +0600

    sum with loop

---
+++ 03-Loop/loop/sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    int sum = 0;
+    int n;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i += 1)
+    {
+        sum += i;
+    }
+    printf("%d \n", sum);
+    return 0;
+}
