99e11f03d1066e98499813d3bf1f54d5ec8abc7c
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-10 17:31:11 +0600

    reverse problem solved

---
+++ 05.5-Eid-Vacation-Practice-Set/c-project/reverse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int reversed = 0;
+    while (N > 0)
+    {
+        int digit = N % 10;
+        reversed = reversed * 10 + digit;
+        N /= 10;
+    }
+    printf("%d", reversed);
+    return 0;
+}
