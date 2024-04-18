7268ca85bdcdd0eb1a4940a684d2e66f77f07968
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-18 18:04:48 +0600

    summation problem solved

---
+++ 07.5-Practice-Day/c-project/A_Summation.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    long long int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    if (sum < 0)
+    {
+        sum *= -1;
+    }
+
+    printf("%lld", sum);
+    return 0;
+}
