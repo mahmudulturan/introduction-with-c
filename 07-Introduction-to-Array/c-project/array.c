98b9f9e65841e1a4c06e05ec650d3fcdca22c0be
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-18 17:37:34 +0600

    reverse print in array

--- 07-Introduction-to-Array/c-project/array.c
+++ 07-Introduction-to-Array/c-project/array.c
@@ -13,7 +13,7 @@ int main()
     int arr2[3] = {15, 785};
     for (int i = 0; i < 3; i++) // show output
     {
-        printf("%d ", arr2[i]); // 15 785 0
+        printf("%d ", arr2[i]); // 15 785 0 last element will show 0 because we dont give value in the last element
     }
     return 0;
 }
