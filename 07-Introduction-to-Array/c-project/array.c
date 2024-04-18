a753d6e26039ae8d959158346c04aeb1a6f901b0
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-18 17:24:04 +0600

    input and output in array

--- 07-Introduction-to-Array/c-project/array.c
+++ 07-Introduction-to-Array/c-project/array.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int arr[5]; // declare an array with limit 5
+    int arr[5];                 // declare an array with limit 5
+    for (int i = 0; i < 5; i++) // take input
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < 5; i++) // show output
+    {
+        printf("%d\n", arr[i]);
+    }
     return 0;
 }
