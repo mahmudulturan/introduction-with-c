7ea5f7c5c5b35560f6fcea8252f3791877ee9f19
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-25 19:09:04 +0600

    k_sum_digits problem solved

---
+++ 11.5-Practice-Day/c-project/K_Sum_Digits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char numbers[n];
+    scanf("%s", numbers);
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += numbers[i] - '0';
+    }
+    printf("%d\n", sum);
+    return 0;
+}
