77c0cafaf2e303a8836eddedd38f185134c96aad
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-25 17:46:29 +0600

    counting number frequency array

--- 11-String-Operation-&-Frequency-Array/c-project/counting_array.c
+++ 11-String-Operation-&-Frequency-Array/c-project/counting_array.c
@@ -6,29 +6,15 @@ int main()
     {
         scanf("%d", &arr[i]);
     }
-    int count0 = 0, count1 = 0, count2 = 0, count3 = 0;
+    int count[8] = {0};
     for (int i = 0; i < 8; i++)
     {
-        if (arr[i] == 0)
-        {
-            count0++;
-        }
-        if (arr[i] == 1)
-        {
-            count1++;
-        }
-        if (arr[i] == 2)
-        {
-            count2++;
-        }
-        if (arr[i] == 3)
-        {
-            count3++;
-        }
+        int number = arr[i];
+        count[number]++;
     }
-    printf("0 - %d times\n", count0);
-    printf("1 - %d times\n", count1);
-    printf("2 - %d times\n", count2);
-    printf("3 - %d times\n", count3);
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%d - %d times\n", i, count[i]);
+    }
     return 0;
 }
