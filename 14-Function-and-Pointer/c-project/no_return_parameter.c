020a32f50c5e3a7c3df4cbe947aae5773ca66e49
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-30 13:13:50 +0600

    no return + parameter

---
+++ 14-Function-and-Pointer/c-project/no_return_parameter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void sum(int a, int b)
+{
+    int result = a + b;
+    printf("%d", result);
+}
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    sum(a, b);
+    return 0;
+}
