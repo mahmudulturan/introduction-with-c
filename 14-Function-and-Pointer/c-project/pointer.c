196c2f9c62df64cdf654e83f7c8c1650e9890ef2
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-30 13:48:19 +0600

    pointer

--- 14-Function-and-Pointer/c-project/pointer.c
+++ 14-Function-and-Pointer/c-project/pointer.c
@@ -3,7 +3,11 @@ int main()
 {
     int x = 54;
     printf("%p\n", &x); // to print an address we need to use %p
-    int*xAddress = &x;
-    printf("%p\n", xAddress); // to print an address we need to use %p
+    int *xAddress = &x;
+    printf("%p\n", xAddress);  // to print an address we need to use %p
+    printf("%d\n", *xAddress); // to print the variable by dereference
+    *xAddress = 55;            // to change the value of the variable by dereference
+    printf("%d\n", *xAddress); // to print the variable by dereference
+
     return 0;
 }
