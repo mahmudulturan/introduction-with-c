4bb3b618ff81ea4abb7a1090675605e3b9e48699
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-30 13:26:08 +0600

    some built in func

---
+++ 14-Function-and-Pointer/c-project/built_in_func.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include<stdlib.h>
+int main()
+{
+    printf("%llf\n", ceil(4.3));
+    printf("%llf\n", floor(4.3));
+    printf("%llf\n", round(4.5));
+    printf("%llf\n", sqrt(9));
+    printf("%llf\n", pow(3,3));
+    printf("%d\n", abs(-7));
+    return 0;
+}
